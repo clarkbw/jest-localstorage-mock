414e12181fdd74f1cb3cfa99a6ac743d7f121e9f
Bryan Clark <clarkbw@gmail.com>
2017-06-08 08:29:03 -0700

    Include jest mocks for each localStorage function call

--- src/setup.js
+++ src/setup.js
@@ -1,3 +1,21 @@
 import { LocalStorage } from './localstorage';
 
-global.localStorage = new LocalStorage();
+const local = new LocalStorage();
+
+global.localStorage = {
+  clear: jest.fn(() => local.clear()),
+  getItem: jest.fn(key => local.getItem(key)),
+  setItem: jest.fn((key, value) => local.setItem(key, value)),
+  removeItem: jest.fn(key => local.removeItem(key)),
+  key: jest.fn(index => local.key(index)),
+  toString: jest.fn(() => local.toString()),
+  get __STORE__() {
+    return local.store;
+  },
+  set __STORE__(store) {
+    local.store = store;
+  },
+  get length() {
+    return local.length;
+  },
+};
