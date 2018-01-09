c85d00cbcbca1753ffd91758049e8612bcd12f38
Brian McCall <mccabj0210@gmail.com>
2018-01-09 09:20:44 -0800

    make properties non-enumerable, they should be separate from the localStorage storage object

--- src/setup.js
+++ src/setup.js
@@ -3,38 +3,7 @@ import { LocalStorage } from './localstorage';
 const local = new LocalStorage();
 const session = new LocalStorage();
 
-global.localStorage = {
-  clear: jest.fn(() => local.clear()),
-  getItem: jest.fn(key => local.getItem(key)),
-  setItem: jest.fn((key, value) => local.setItem(key, value)),
-  removeItem: jest.fn(key => local.removeItem(key)),
-  key: jest.fn(index => local.key(index)),
-  toString: jest.fn(() => local.toString()),
-  get __STORE__() {
-    return local.store;
-  },
-  set __STORE__(store) {
-    local.store = store;
-  },
-  get length() {
-    return local.length;
-  },
-};
+global.localStorage = local;
+global.sessionStorage = session;
 
-global.sessionStorage = {
-  clear: jest.fn(() => session.clear()),
-  getItem: jest.fn(key => session.getItem(key)),
-  setItem: jest.fn((key, value) => session.setItem(key, value)),
-  removeItem: jest.fn(key => session.removeItem(key)),
-  key: jest.fn(index => session.key(index)),
-  toString: jest.fn(() => session.toString()),
-  get __STORE__() {
-    return session.store;
-  },
-  set __STORE__(store) {
-    session.store = store;
-  },
-  get length() {
-    return session.length;
-  },
-};
+
