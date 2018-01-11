919b38e19753d91354fb415991759ae461355a48
Brian McCall <mccabj0210@gmail.com>
2018-01-10 17:58:10 -0800

    run build

--- dist/setup.js
+++ dist/setup.js
@@ -1,59 +1,51 @@
 'use strict';
 
 class LocalStorage {
-    constructor() {
-        Object.defineProperty(this, "getItem", {
-            enumerable: false,
-            value: jest.fn(key => {
-                return this[key] || null;
-            })
+  constructor(jest) {
+    Object.defineProperty(this, 'getItem', {
+      enumerable: false,
+      value: jest.fn(key => this[key] || null)
+    });
+    Object.defineProperty(this, 'setItem', {
+      enumerable: false,
+      // not mentioned in the spec, but we must always coerce to a string
+      value: jest.fn((key, val = '') => {
+        this[key] = val + '';
+      })
+    });
+    Object.defineProperty(this, 'removeItem', {
+      enumerable: false,
+      value: jest.fn(key => {
+        delete this[key];
+      })
+    });
+    Object.defineProperty(this, 'clear', {
+      enumerable: false,
+      value: jest.fn(() => {
+        Object.keys(this).map(key => delete this[key]);
+      })
+    });
+    Object.defineProperty(this, 'toString', {
+      enumerable: false,
+      value: jest.fn(() => {
+        return '[object Storage]';
+      })
+    });
+    Object.defineProperty(this, 'key', {
+      enumerable: false,
+      value: jest.fn(idx => Object.keys(this)[idx] || null)
+    });
+  } // end constructor
 
-        });
-        Object.defineProperty(this, "setItem", {
-            enumerable: false,
-            value: jest.fn((key, val = '') => {
-                this[key] = val + '';
-            })
-        });
-        Object.defineProperty(this, "removeItem", {
-            enumerable: false,
-            value: jest.fn(key => {
-                delete this[key];
-            })
-        });
-        Object.defineProperty(this, "clear", {
-            enumerable: false,
-            value: jest.fn(() => {
-                Object.keys(this).map(key => delete this[key]);
-            })
-        });
-        Object.defineProperty(this, "toString", {
-            enumerable: false,
-            value: jest.fn(() => {
-                return "[object Storage]";
-            })
-        });
-        Object.defineProperty(this, "key", {
-            enumerable: false,
-            value: jest.fn(idx => {
-                return Object.keys(this)[idx] || null;
-            })
-        });
-    } // end constructor
-
-    get length() {
-        return Object.keys(this).length;
-    }
-
-    get __STORE__() {
-        return this;
-    }
-
+  get length() {
+    return Object.keys(this).length;
+  }
+  // for backwards compatibility
+  get __STORE__() {
+    return this;
+  }
 }
 
-const local = new LocalStorage();
-const session = new LocalStorage();
-
-global.localStorage = local;
-global.sessionStorage = session;
+global.localStorage = new LocalStorage(jest);
+global.sessionStorage = new LocalStorage(jest);
 //# sourceMappingURL=setup.js.map
