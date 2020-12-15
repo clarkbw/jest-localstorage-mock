9bcc9b3a50d0b94d33d82a766474b802e482ccdf
Zach <12666484+ZachMayry@users.noreply.github.com>
2020-12-15 11:07:38 -0600

    fix: empty string no longer coalesces into null (#121)
    * fix: empty string no longer coalesces into null
    
    * fix: empty string no longer coalesces into null
    
    Co-authored-by: Zachary Mayry <zachary.mayry@target.com>
    Co-authored-by: Bryan Clark <clarkbw@github.com>

---
+++ __tests__/localstorage.test.js
@@ -0,0 +1,20 @@
+import { LocalStorage } from "../src/localstorage";
+
+describe("localstorage", () => {
+  let localStorage;
+
+  beforeEach(() => {
+    localStorage = new LocalStorage(jest);
+  });
+
+  describe("getItem", () => {
+    it("should return null if the item is undefined", () => {
+      expect(localStorage.getItem("item")).toBeNull();
+    });
+
+    it("should return '' instead of null", () => {
+      localStorage.setItem("item", "");
+      expect(localStorage.getItem("item")).toBe("");
+    });
+  });
+});
